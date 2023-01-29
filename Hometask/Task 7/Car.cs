52050fd92898ee0829fc1353e8ad17421c806241
Aydanshj <121218604+Aydanshj@users.noreply.github.com>
2023-01-29 23:04:52 +0400

    Add files via upload

---
+++ Hometask/Task 7/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hometask8
+{
+    internal class Car:Vehicle
+    {
+        public Car(string Brand, string Model, double Millage, double FuelCapacity, double CurrentFuel) : base(Brand, Model, Millage)
+        {
+            this.CurrentFuel = CurrentFuel;
+            this.FuelCapacity = FuelCapacity;
+        }
+
+        public double FuelCapacity;
+        public double CurrentFuel;
+
+        public void AddFuel(double Lt)
+        {
+            if(CurrentFuel + Lt <= FuelCapacity)
+            {
+                CurrentFuel += Lt;
+            }
+        }
+    }
+}
---
+++ Hometask/Task 7/Human.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hometask8
+{
+    internal class Human
+    {
+        public string Fullname;
+    }
+}
